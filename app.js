75a7d9fc8f4a0dfd6789fdc0661b1d39800fca15
Ivans Aleskevics <vanjaaleskevic@mail.ru>
2021-03-16 14:57:11 +0200

    lesson17

--- app.js
+++ app.js
@@ -45,16 +45,32 @@ hideBox.addEventListener('change', function(e){
 });
 
 // filter books
-const searchBar = document.forms['search-books'].querySelector('input');
-searchBar.addEventListener('keyup', function(e){
+const searchBar = forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', (e) => {
   const term = e.target.value.toLowerCase();
   const books = list.getElementsByTagName('li');
-  Array.from(books).forEach(function(book){
+  Array.from(books).forEach((book) => {
     const title = book.firstElementChild.textContent;
-    if(title.toLowerCase().indexOf(term) != -1) {
+    if(title.toLowerCase().indexOf(e.target.value) != -1){
       book.style.display = 'block';
     } else {
       book.style.display = 'none';
     }
-  })
-}) 
+  });
+});
+
+// tabbed content
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', (e) => {
+  if(e.target.tagName == 'LI'){
+    const targetPanel = document.querySelector(e.target.dataset.target);
+    Array.from(panels).forEach((panel) => {
+      if(panel == targetPanel){
+        panel.classList.add('active');
+      }else{
+        panel.classList.remove('active');
+      }
+    });
+  }
+});
